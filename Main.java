e6b09ff27b76f15e050fb0fe5cc8bc1b7a199c8a
Shogo Hosoda <s.hosoda.sys21@morijyobi.ac.jp>
2022-03-17 12:28:05 +0900

    BMI出力を2回繰り返すように変更

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 	  Scanner sc = new Scanner(System.in);
+	  for(int i = 0; i < 2 ; i++ ){
 	  System.out.print("height>");
 	  double h = sc.nextDouble();
 	  System.out.print("weight>");
@@ -10,5 +11,6 @@ public class Main {
           h = h / 100; 
           double bmi = w / (h * h);
           System.out.println("bmi>" + bmi);
+	  }
 	}
 }
